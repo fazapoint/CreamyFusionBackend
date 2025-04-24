ee2ec4a8e9c253dd84599c24037825b197275160
fazaramadhani99 gmail.com <fazaramadhani99@gmail.com>
2025-04-24 06:26:06 +0000

    add deleted to product model

--- CreamyFusion/Models/Product.cs
+++ CreamyFusion/Models/Product.cs
@@ -4,6 +4,7 @@
     {
         public Guid Id { get; set; } = Guid.NewGuid();
         public string Name { get; set; }
+        public bool Deleted { get; set; }
 
         // Navigation property for the one-to-many relationship with ProductPrices
         public ICollection<ProductPrice> ProductPrices { get; set; }
