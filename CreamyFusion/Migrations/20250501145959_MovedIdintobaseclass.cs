4be53b90481a19ef7e131154ad5cca969ec2d583
fazapoint <fazaramadhani99@gmail.com>
2025-05-01 22:00:29 +0700

    add migrations

---
+++ CreamyFusion/Migrations/20250501145959_MovedIdintobaseclass.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CreamyFusion.Migrations
+{
+    /// <inheritdoc />
+    public partial class MovedIdintobaseclass : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Modified",
+                table: "Products",
+                type: "datetime2",
+                nullable: false,
+                defaultValueSql: "GETUTCDATE()",
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Created",
+                table: "Products",
+                type: "datetime2",
+                nullable: false,
+                defaultValueSql: "GETUTCDATE()",
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Modified",
+                table: "ProductPrices",
+                type: "datetime2",
+                nullable: false,
+                defaultValueSql: "GETUTCDATE()",
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Created",
+                table: "ProductPrices",
+                type: "datetime2",
+                nullable: false,
+                defaultValueSql: "GETUTCDATE()",
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Modified",
+                table: "Products",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValueSql: "GETUTCDATE()");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Created",
+                table: "Products",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValueSql: "GETUTCDATE()");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Modified",
+                table: "ProductPrices",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValueSql: "GETUTCDATE()");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Created",
+                table: "ProductPrices",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldDefaultValueSql: "GETUTCDATE()");
+        }
+    }
+}
